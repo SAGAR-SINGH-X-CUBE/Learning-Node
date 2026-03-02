e94aaa0a8208f9c51b40a9692f3693a391d37750
SAGAR-SINGH-X-CUBE <Sagarsinghxcube@gmail.com>
2026-03-02 23:05:53 +0530

    data route added

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/data',(req,res)=>{
+  res.json({
+    name: "Sagar",
+    age: 22,
+    course: "Node.js"
+  })
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
